fc1f0609664975b8f6d9f5ebc0a0934243fe2718
Pele <bennett.johanson@gmail.com>
2019-09-16 16:41:36 -0700

    connected to MongoDB

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://Devadmin:Team3@instaminicluster-zry5f.mongodb.net/test?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
+//Db config
+const db = require("./config/keys").mongoURI;
 
+//connect to mongodb
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 //First Route
-app.get('/', (req,res) => res.send('hello team 3!'));
+app.get("/", (req, res) => res.send("hello team 3!"));
 
 const port = 5104;
 
