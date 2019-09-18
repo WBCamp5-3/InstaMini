5537f5f150b68ad6f97224545dda43963c1be2e9
Tracy Miller <tlmiller36@gmail.com>
2019-09-18 14:54:25 -0700

    add routes folder, import and route in server.js

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+
 //Db config
 const db = require("./config/keys").mongoURI;
 
@@ -13,6 +18,13 @@ mongoose
 
 //First Route
 app.get("/", (req, res) => res.send("hello team 3!"));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
+
 
 const port = 5104;
 
