91bfe6b9745dbcb4afc29535c55d1a9f77dde655
Pele <bennett.johanson@gmail.com>
2019-09-24 17:07:52 -0700

    thru 9.14 video, ~39:00

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,125 +1,20 @@
 const express = require("express");
-
-//We don't need all of express in this file, just the Router
 const router = express.Router();
-
-const User = require("../../models/User");
-const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
-const passport = require("passport");
-const validateRegisterInput = require("../../validation/register");
-const validateLoginInput = require('../../validation/login')
+// bring in User model
+const User = require('../../models/User');
 
 // @route   POST api/users/register
 // @desc    Register a user
 // @access  Public
-router.post("/register", (req, res) => {
-  const { errors, isValid } = validateRegisterInput(req.body);
 
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (user) {
-        errors.email = "Email already exists";
-        return res.status(400).json(errors);
-      } else {
-        const avatar = gravatar.url(req.body.email, {
-          s: "200",
-          r: "pg",
-          d: "mm"
-        });
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          avatar,
-          password: req.body.password
-        });
-
-        //separate function
-        bcrypt.genSalt(10, (err, salt) => {
-          if (err) throw err;
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
-          });
-        });
-      }
-    })
-    .catch(err => console.log(err));
-});
-
-// @route   POST api/users/login
-// @desc    Login user / return JWT token
-// @access  Public
-
-router.post("/login", (req, res) => {
-  const { errors, isValid } = validateLoginInput(req.body);
-
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-  const email = req.body.email;
-  const password = req.body.password;
-
-  //Find user by email
-  User.findOne({ email })
-    .then(user => {
-      if (!user) {
-        errors.email = "User not found"
-        return res.status(404).json(errors);
-      }
-      bcrypt
-        .compare(password, user.password)
-        .then(isMatch => {
-          if (!isMatch) {
-            errors.password = "Password does not match"
-            return res.status(400).json(errors);
-          }
-          const payload = {
-            id: user.id,
-            name: user.name,
-            avatar: user.avatar
-          };
-          jwt.sign(
-            payload,
-            keys.secretOrKey,
-            { expiresIn: 3600 },
-            (err, token) => {
-              if (err) throw err;
-
-              return res.json({
-                success: true,
-                token: "Bearer " + token
-              });
-            }
-          );
-        })
-        .catch(err => console.log(err));
-    })
-    .catch(err => console.log(err));
-});
-
-// @route   GET api/users/current
-// @desc    return current user info
-// @access  Private
-router.get(
-  "/current",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    res.json({
-      id: req.user.id,
-      email: req.user.email,
-      name: req.user.name
-    });
-  }
-);
+// this route is called when user hits submit button
+// Express is already set up in server.js to use bodyParser in json format
+// So, req will already be parsed as key value pair
+// findOne is built in function of MongoDb
+router.post('/register', (req, res)=>{
+  User.findOne({email: req.body.email})
+  .then()
+  .catch()
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+const bodyParser = require("body-parser");
 
+// Body parser middleware
+// Telling Express to use bodyParser
+// extended false is default form of encoding
+app.use(bodyParser.urlencoded({extended: false}));
+// Telling bodyParser we're using json
+app.use(bodyParser.json());
 
 //Db config
 const db = require("./config/keys").mongoURI;
@@ -19,11 +26,9 @@ mongoose
 //First Route
 app.get("/", (req, res) => res.send("hello team 3!"));
 
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
-
-
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = 5104;
 
