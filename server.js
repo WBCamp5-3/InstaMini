a1a8ccaac4c54ba95dc85a348e5aba9ecc62b6a9
Tracy Miller <tlmiller36@gmail.com>
2019-09-27 14:36:33 -0700

    ?

--- server.js
+++ server.js
@@ -2,10 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
+// Body parser middleware
+// Telling Express to use bodyParser
+// extended false is default form of encoding
+app.use(bodyParser.urlencoded({extended: false}));
+// Telling bodyParser we're using json
+app.use(bodyParser.json());
+
+
+//Passport configuration
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 //Db config
 const db = require("./config/keys").mongoURI;
@@ -19,11 +32,9 @@ mongoose
 //First Route
 app.get("/", (req, res) => res.send("hello team 3!"));
 
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
-
-
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = 5104;
 
