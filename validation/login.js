49059f461f642fcdf18038d2d469f913b3c11bcc
Pele <bennett.johanson@gmail.com>
2019-09-28 13:02:56 -0700

    Email empty for login fixed

--- validation/login.js
+++ validation/login.js
@@ -1,28 +1,23 @@
-const Validator = require('validator');
-const isEmpty =require('./is-empty');
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
 
+module.exports = function validateLoginInput(data) {
+  let errors = {};
 
-module.exports = function validateLoginInput(data){
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
 
-  let errors ={};
-
-  data.email= !isEmpty(data.email)? data.email : '';
-  data.password = !isEmpty(data.password)? data.password: '';
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
 
   if (Validator.isEmpty(data.email)) {
     errors.email = "Email is required";
-  }
-  if (!Validator.isEmail(data.email)) {
-    errors.email = "Email is invalid";
   }
 
   if (Validator.isEmpty(data.password)) {
     errors.password = " Password is required";
   }
 
-  
-
-  return {errors, isValid:isEmpty(errors)}
-
-}
-
+  return { errors, isValid: isEmpty(errors) };
+};
