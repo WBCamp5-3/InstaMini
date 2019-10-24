9066b0ebae9425aefc89f338b3400d7a56b6262a
Kristen Johanson <43449427+klbjklbj@users.noreply.github.com>
2019-10-24 11:39:51 -0700

    changed 'avatar' on client side to 'profilePicture' to match server side

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -38,7 +38,7 @@ class Navbar extends Component {
           >
             <img
               className="rounded-circle"
-              src={user.avatar}
+              src={user.profilePicture}
               alt={user.name}
               style={{ width: "25px", marginRight: "5px" }}
               title="You must have a Gravatar connected to your email to display an image"
