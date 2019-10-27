3bec6a9466f1ba4c7545b7827ae8e76f84ac6c6d
Kristen Johanson <43449427+klbjklbj@users.noreply.github.com>
2019-10-26 17:43:31 -0700

    Changed status field to input box

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -75,19 +75,6 @@ class CreateProfile extends Component {
   render() {
     const { errors } = this.state;
 
-    // Select options for status
-    const options = [
-      { label: "* Select Professional Status", value: 0 },
-      { label: "Developer", value: "Developer" },
-      { label: "Junior Developer", value: "Junior Developer" },
-      { label: "Senior Developer", value: "Senior Developer" },
-      { label: "Manager", value: "Manager" },
-      { label: "Student or Learning", value: "Student or Learning" },
-      { label: "Instructor or Teacher", value: "Instructor or Teacher" },
-      { label: "Intern", value: "Intern" },
-      { label: "Other", value: "Other" }
-    ];
-
     return (
       <div className="create-profile">
         <div className="container">
@@ -107,12 +94,11 @@ class CreateProfile extends Component {
                   error={errors.handle}
                   info="A unique handle for your profile URL. Your full name, company name, nickname"
                 />
-                <SelectListGroup
+                <TextFieldGroup
                   placeholder="Status"
                   name="status"
                   value={this.state.status}
                   onChange={this.onChange}
-                  options={options}
                   error={errors.status}
                   info="Give us an idea of where you are at in your career"
                 />
