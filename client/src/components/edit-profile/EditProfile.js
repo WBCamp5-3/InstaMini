398baa03f9e4e2e04b0397f47f7bba4cd3999c84
balaparameswari45 <52223968+balaparameswari45@users.noreply.github.com>
2019-10-28 21:47:11 -0700

    Updated the server validation profile with respect to edit profile

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -11,7 +11,7 @@ class EditProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: "",
+      handle: "",
       website: "",
       location: "",
       bio: "",
@@ -39,7 +39,7 @@ class EditProfile extends Component {
       const profile = nextProps.profile;
 
       // If profile field doesnt exist, make empty string
-      profile.userName = !isEmpty(profile.userName) ? profile.userName : "";
+      profile.handle = !isEmpty(profile.handle) ? profile.handle : "";
       profile.location = !isEmpty(profile.location) ? profile.location : "";
       profile.status = !isEmpty(profile.status) ? profile.status : "";
 
@@ -47,7 +47,7 @@ class EditProfile extends Component {
 
       // Set component fields state
       this.setState({
-        userName: profile.userName,
+        handle: profile.handle,
         location: profile.location,
         status: profile.status,
         bio: profile.bio
@@ -59,7 +59,7 @@ class EditProfile extends Component {
     e.preventDefault();
 
     const profileData = {
-      userName: this.state.userName,
+      handle: this.state.handle,
       location: this.state.location,
       status: this.state.status,
       bio: this.state.bio
@@ -76,75 +76,68 @@ class EditProfile extends Component {
     const { errors } = this.state;
 
     return (
-      <div className="edit-profile">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 m-auto">
-              <Link to="/dashboard" className="btn btn-light">
-                Go Back
-              </Link>
-              <h1 className="display-4 text-center">Edit Profile</h1>
-              <small className="d-block pb-3">* = required fields</small>
-              <form onSubmit={this.onSubmit}>
-                <TextFieldGroup
-                  placeholder="* Profile Username"
-                  name="username"
-                  value={this.state.userName}
-                  onChange={this.onChange}
-                  error={errors.userName}
-                  info=" Your username"
-                />
-                <TextFieldGroup
-                  placeholder="Status"
-                  name="status"
-                  value={this.state.status}
-                  onChange={this.onChange}
-                  error={errors.status}
-                  info="Give us an idea of where you are at in your career"
-                />
-                <TextFieldGroup
-                  placeholder="Company"
-                  name="company"
-                  value={this.state.company}
-                  onChange={this.onChange}
-                  error={errors.company}
-                  info="Could be your own company or one you work for"
-                />
-                <TextFieldGroup
-                  placeholder="Website"
-                  name="website"
-                  value={this.state.website}
-                  onChange={this.onChange}
-                  error={errors.website}
-                  info="Could be your own website or a company one"
-                />
-                <TextFieldGroup
-                  placeholder="Location"
-                  name="location"
-                  value={this.state.location}
-                  onChange={this.onChange}
-                  error={errors.location}
-                  info="City or city & state suggested (eg. Boston, MA)"
-                />
-                <TextAreaFieldGroup
-                  placeholder="Short Bio"
-                  name="bio"
-                  value={this.state.bio}
-                  onChange={this.onChange}
-                  error={errors.bio}
-                  info="Tell us a little about yourself"
-                />
-                <input
-                  type="submit"
-                  value="Submit"
-                  className="btn btn-info btn-block mt-4"
-                />
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+			<div className="edit-profile">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-8 m-auto">
+							<Link to="/dashboard" className="btn btn-light">
+								Go Back
+							</Link>
+							<h1 className="display-4 text-center">Edit Profile</h1>
+							<small className="d-block pb-3">* = required fields</small>
+							<form onSubmit={this.onSubmit}>
+								<TextFieldGroup
+									placeholder="* Profile handle"
+									name="handle"
+									value={this.state.handle}
+									onChange={this.onChange}
+									error={errors.handle}
+									info="Your username"
+								/>
+								<TextFieldGroup
+									placeholder="Status"
+									name="status"
+									value={this.state.status}
+									onChange={this.onChange}
+									error={errors.status}
+									info="Give us an idea of what is your interest"
+								/>
+
+								<TextFieldGroup
+									placeholder="Website"
+									name="website"
+									value={this.state.website}
+									onChange={this.onChange}
+									error={errors.website}
+									info="Could be your own website or a company one"
+								/>
+								<TextFieldGroup
+									placeholder="Location"
+									name="location"
+									value={this.state.location}
+									onChange={this.onChange}
+									error={errors.location}
+									info="City or city and state suggested (eg. Boston, MA)"
+								/>
+								<TextAreaFieldGroup
+									placeholder="Short Bio"
+									name="bio"
+									value={this.state.bio}
+									onChange={this.onChange}
+									error={errors.bio}
+									info="Tell us a little about yourself"
+								/>
+								<input
+									type="submit"
+									value="Submit"
+									className="btn btn-info btn-block mt-4"
+								/>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
   }
 }
 
