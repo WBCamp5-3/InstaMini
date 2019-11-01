5548a0cb0790b9388f3e69f7a4844403f9db95a2
Kristen Johanson <43449427+klbjklbj@users.noreply.github.com>
2019-11-01 16:24:28 -0700

    first image shows up in profile

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -6,12 +6,16 @@ class ProfileAbout extends Component {
   render() {
     const { profile } = this.props;
     console.log(profile);
+    console.log(profile.posts[0].image);
 
     return (
       <div className="row">
         <div className="col-md-12">
           <div className="card card-body bg-light mb-3">
-            <h3 className="text-center text-info">{profile.user.userName}'s Bio</h3>
+            <h3 className="text-center text-info">
+              {profile.user.userName}'s Bio
+            </h3>
+
             <p className="lead">
               {isEmpty(profile.bio) ? (
                 <span>{profile.user.userName} does not have a bio</span>
@@ -20,7 +24,27 @@ class ProfileAbout extends Component {
               )}
             </p>
             <hr />
-            
+          </div>
+
+          <div className="card card-body bg-light mb-3">
+            <h3 className="text-center text-info">
+              {profile.user.userName}'s Posts
+            </h3>
+
+            <p className="lead">
+              {isEmpty(profile.posts[0].image) ? (
+                <span>{profile.user.userName} does not have any posts</span>
+              ) : (
+                // NEED TO LOOP THROUGH ARRAY OF IMAGES
+                <span>
+                  <img
+                    src={profile.posts[0].image}
+                    style={{ width: 200 }}
+                  />
+                </span>
+              )}
+            </p>
+            <hr />
           </div>
         </div>
       </div>
