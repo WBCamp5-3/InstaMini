a6e17d02fe44e888967bc52b7f41118a3eb73ae1
balaparameswari45 <52223968+balaparameswari45@users.noreply.github.com>
2019-11-02 14:19:39 -0700

    Final commit

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const profile = require("./routes/api/profile");
 const following = require("./routes/api/following");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path =require('path');
 
 // Body parser middleware
 // Telling Express to use bodyParser
@@ -30,12 +31,18 @@ mongoose
   .catch(err => console.log(err));
 
 //First Route
-app.get("/", (req, res) => res.send("hello team 3!"));
+// app.get("/", (req, res) => res.send("hello team 3!"));
 
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/following", following);
 
-const port = 5104;
+if(PROCESS.env.NODE_ENV==='production'){
+  app.use(express.static('client/build'));
+  app.get('*', (req,res) => { res.sendFile(path.resolve(__dirname,'client','build', 'index.html'));
+})
+}
+
+const port = process.env.PORT || 5104;
 
 app.listen(port, () => console.log(`Server running on ${port}`));
