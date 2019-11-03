7efdf05bd47b4b4631e054056f134eb6222d47e9
balaparameswari45 <52223968+balaparameswari45@users.noreply.github.com>
2019-11-02 20:52:44 -0700

    PROCESS to process

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/following", following);
 
-if(PROCESS.env.NODE_ENV==='production'){
+if(process.env.NODE_ENV ==='production'){
   app.use(express.static('client/build'));
   app.get('*', (req,res) => { res.sendFile(path.resolve(__dirname,'client','build', 'index.html'));
 })
